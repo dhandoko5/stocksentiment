609169f37658e1745cde1c4d073cf75f7167af07
Hassan Nasif <hrnasif@gmail.com>
2016-07-20 18:11:58 -0400

    Fixed import issue

--- R/cleanup.R
+++ R/cleanup.R
@@ -2,10 +2,11 @@ cleanup <- function(text) {
   text= gsub("[^[:print:]]", "", text)
   text = gsub("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", text)
   text = gsub("[^[:space:]]*…$", "", text)
-  text = stripWhitespace(text)
+  text = tm::stripWhitespace(text)
   text = tolower(text)
-  text = removeWords(text, words= stopwords("english"))
-  text = removePunctuation(text)
-  text = removeNumbers(text)
+  text = tm::removeWords(text, words= stopwords("english"))
+  text = tm::removePunctuation(text)
+  text = tm::removeNumbers(text)
   return(text)
 }
+
