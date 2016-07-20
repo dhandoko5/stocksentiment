cef025d9753847fa1a123977208ea3445e9dfe69
Darren Handoko <drhandoko@gmail.com>
2016-07-21 00:39:18 +0200

    4th commit

--- R/cleanup.R
+++ R/cleanup.R
@@ -2,11 +2,11 @@ cleanup <- function(text) {
   text= gsub("[^[:print:]]", "", text)
   text = gsub("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", text)
   text = gsub("[^[:space:]]*…$", "", text)
-  text = tm::stripWhitespace(text)
   text = tolower(text)
   text = tm::removeWords(text, words= tm::stopwords("english"))
   text = tm::removePunctuation(text)
   text = tm::removeNumbers(text)
+  text = tm::stripWhitespace(text)
   return(text)
 }
 
